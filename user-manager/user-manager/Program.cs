f1950a9129bfff9beffcd0011ddb9a8e9e1da327
albin86 <55479592+albin86@users.noreply.github.com>
2019-09-18 12:13:35 +0200

    greeted user and added menu

--- user-manager/user-manager/Program.cs
+++ user-manager/user-manager/Program.cs
@@ -6,7 +6,27 @@ namespace user_manager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Show main menu");
+        }
+        /// <summary>
+        /// Shows main menu
+        /// </summary>
+        /// <returns>Selected menu</returns>
+        static int ShowMainMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine(" Welcome to user manager");
+            Console.WriteLine("1 Create a user");
+            Console.WriteLine("2 Show all user");
+            Console.WriteLine("3 Mail all users");
+            Console.WriteLine("4 Exit");
+            Console.WriteLine("Select an option: ");
+
+            string input = Console.ReadLine();
+            int selectedOption = int.Parse(input);
+
+            return selectedOption;
         }
     }
 }
